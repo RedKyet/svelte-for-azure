bb9d49190311d7eb3ffbf708b0852085c88ab120
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2022-11-24 18:34:39 +0200

    added adapter for azure

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+import azure from 'svelte-adapter-azure-swa';
+
+export default {
+	kit: {
+		...
+		adapter: azure()
+	}
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
